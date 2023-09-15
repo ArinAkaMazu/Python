280c3aab22dcd690dd6f858962a4e267f274af1e
ArinAkaMazu <arinkaushik60@gmail.com>
2023-09-15 18:09:26 +0530

    first program

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+num=int(input("enter a value to be checked"))
+temp=num
+sum=0
+while(num>0):
+    rem=num%10
+    sum=sum+ rem*rem*rem
+    num=num//10
+if(sum==temp):
+ print("is armstrong")
+else:
+ print("not")
