7fd99f1f131effe66ecc7880c6bc02c7fb048b92
Arin <arinkaushik60@gmail.com>
2023-09-22 10:21:31 +0530

    new file

---
+++ date.py
@@ -0,0 +1,33 @@
+mydate=input("enter a date(mm/dd/yyyy):")
+datevalue=mydate.split('/')
+month = datevalue[0]
+date = datevalue[1]
+year = datevalue[2]
+if month=='01':
+    month='January'
+elif month=='02':
+    month='Febuary'
+elif month=='03':
+    month='March'
+elif month=='04':
+    month='April'
+elif month=='05':
+    month='May'
+elif month=='06':
+    month='June'
+elif month=='07':
+    month='July'
+elif month=='08':
+    month='August'
+elif month=='09':
+    month='Septmber'
+elif month=='10':
+    month='October'
+elif month=='11':
+    month='November'
+elif month=='12':
+    month='December'
+else:
+    print("invalid input")
+newdate=month,str(date),str(year)
+print(newdate)
