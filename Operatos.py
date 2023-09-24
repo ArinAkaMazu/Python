fb7a0fc0f7fad1bfe8cd0c3e554d7c46a4d3e2e2
ArinAkaMazu <arinkaushik60@gmail.com>
2023-09-24 15:30:14 +0530

    new file

---
+++ Operatos.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 == 0:
+        result = "Cannot divide by zero"
+    else:
+        result = num1 / num2
+else:
+    result = "Invalid operator"
+print("Result: ",result)
