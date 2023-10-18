b118edc00d80bf0fe2c76fa0a8f3263d60dbb3f7
Arin <arinkaushik60@gmail.com>
2023-10-18 13:00:14 +0530

    e

--- email regex.py
+++ email regex.py
@@ -1,6 +1,6 @@
 import re
-txt="welcome contact here at: lords.uni@gmail.com"
-pattern=re.findall("[a-zA-Z0-9-._]+@[a-zA-Z0-9]+\.[a-z].*",txt)
+txt="welcome contact here at: lords$uni@gmail.com"
+pattern=re.findall("[a-zA-Z0-9-._$]+@[a-zA-Z0-9]+\.[a-z].*",txt)
 print(pattern)
 if pattern:
     print("yes match")
