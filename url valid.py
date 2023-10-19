fc357fa005035c6287a50dc2fedfd4ad98d567ee
Arin <arinkaushik60@gmail.com>
2023-10-19 14:18:25 +0530

    w

--- url valid.py
+++ url valid.py
@@ -1,6 +1,6 @@
 import re
 txt="for more info you can visit our url: lordsuni.edu"
-pattern=re.search("((https?://)?(www\.)?)?[a-zA-Z0-9\-._]+\.[a-zA-Z]{2,3}",txt)
+pattern=re.findall("((https?://)?(www\.)?)?[a-zA-Z0-9\-._]+\.[a-zA-Z]{2,3}",txt)
 print(pattern)
 if pattern:
     print("yes match")
