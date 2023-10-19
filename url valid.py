c98626bcb97396fd042653e9eafdc276f5854f3d
Arin <arinkaushik60@gmail.com>
2023-10-19 12:42:34 +0530

    updated

---
+++ url valid.py
@@ -0,0 +1,8 @@
+import re
+txt="for more info you can visit our url: https://lordsuni.edu"
+pattern=re.findall("(https?://)?www\.?[a-zA-Z0-9\-._]+\.[a-zA-Z]{2,3}",txt)
+print(pattern)
+if pattern:
+    print("yes match")
+else:
+    print("not match")
